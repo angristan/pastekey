bd489a1c52bc4172caad92a45a94f72abb5dda13
Stanislas Lange <git@slange.me>
2026-07-18 17:35:08 +0200

    Generate Worker source maps

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  environments: {
+    pastekey: {
+      build: { sourcemap: true },
+    },
+  },
   plugins: [react(), cloudflare()],
 });
